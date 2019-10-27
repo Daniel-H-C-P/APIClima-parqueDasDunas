58d29fb9dd1f5e5d6a25e8b7a8a44ee0097c350c
Daniel de Hollanda Cavalcanti Piñeiro <dhcp2013@gmail.com>
2019-10-27 19:24:44 -0300

    lerbd agora altera bd

--- ClimaTempo/lerbd.py
+++ ClimaTempo/lerbd.py
@@ -1,9 +1,9 @@
 #Para mudar de pasta
 import os
-print(os.path.abspath(os.curdir)) #só pra checar
+# print(os.path.abspath(os.curdir)) #só pra checar o dir base
 os.chdir('..')
-print(os.path.abspath(os.curdir)) #só pra checar
-
+# print(os.path.abspath(os.curdir)) #só pra checar o dir acima
+##########################################
 '''
 Para ler diretamente dos modelos:
 
@@ -14,7 +14,7 @@ pessoas = EmailCliente.objects.all()
 print (pessoas)
 
 '''
-
+#########################################
 #Parte que lê o bd em sqlite3:
 
 import sqlite3
@@ -28,7 +28,21 @@ c.execute("SELECT * FROM cadastro_emailcliente WHERE mandado=0 ") #execute -> sq
 lista = c.fetchall() #busca todos os resultados e cria uma lista
 
 for pessoa in lista:
-    print(pessoa)
+    #print(pessoa)
+    #print(pessoa[0])
+    #print(pessoa[1]) #nome
+    #print(pessoa[2]) #contato
+    #print(pessoa[3]) #dia
+    #print(pessoa[4]) #hora
+    
+    ############################## ESTAS DUAS LINHAS MUDAM O BD!!!!
+    if pessoa[1] == 'Exemplo 3':
+    	c.execute('UPDATE cadastro_emailcliente SET nome = "mudei" WHERE id = {}'.format(int(pessoa[0])))
+    	conn.commit()
+    ############################## 
+
+
+
     #comando loop para interagir com os emails
     #pegar nome, contato, dia, hora e interagir com mandarEmail
 
